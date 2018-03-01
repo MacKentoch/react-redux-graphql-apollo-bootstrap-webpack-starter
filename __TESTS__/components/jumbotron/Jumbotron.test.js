598d2c8c5054622245ecb7309d36d31a3257b667
MacKentoch <erwan.datin@gmail.com>
2018-03-01 06:25:08 +0100

    tests

---
+++ __TESTS__/components/jumbotron/Jumbotron.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Jumbotron from '../../../src/app/components/jumbotron/Jumbotron';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+
+describe('Jumbotron component', () => {
+  it('renders as expected', () => {
+    const component = renderer
+      .create(
+        <Jumbotron>
+          <p>an alert</p>
+        </Jumbotron>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
