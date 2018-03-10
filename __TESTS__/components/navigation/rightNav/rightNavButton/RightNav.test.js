17c5b10a4e6148eac9d39cd272d85830fe9e2dfd
MacKentoch <erwan.datin@gmail.com>
2018-03-10 07:41:26 +0100

    tests

---
+++ __TESTS__/components/navigation/rightNav/rightNavButton/RightNav.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import RightNavButton from '../../../../../src/app/components/navigation/rightNav/rightNavButton/RightNavButton';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import { MemoryRouter } from 'react-router';
+import { mount } from 'enzyme';
+
+describe('RightNavButton (navigation) component', () => {
+  it('renders as expected', () => {
+    const props = {
+      onClick: () => {},
+      link: 'link',
+      label: 'label',
+      viewName: '/',
+    };
+    const component = renderer
+      .create(
+        <div>
+          <MemoryRouter>
+            <RightNavButton {...props} />
+          </MemoryRouter>
+        </div>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should call onClick on a click', () => {
+    const onClickMock = jest.fn();
+    const props = {
+      onClick: onClickMock,
+      link: 'link',
+      label: 'label',
+      viewName: '/',
+    };
+
+    const component = mount(
+      <div>
+        <MemoryRouter>
+          <RightNavButton {...props} />
+        </MemoryRouter>
+      </div>,
+    );
+
+    const button = component.find('a');
+    button.simulate('click');
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
